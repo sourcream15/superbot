53bec2b028ca9158d8fa63105f3cfd05a35f2737
sourcream15 <33765382+sourcream15@users.noreply.github.com>
2017-11-17 22:26:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,15 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log('I am ready!');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
-client.login('process.env.BOT_TOKEN');
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
