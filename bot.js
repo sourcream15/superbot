37e9aa68a6f5f1a4203bac8d846b84db5e83d2b9
sourcream15 <33765382+sourcream15@users.noreply.github.com>
2017-11-18 10:48:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,21 @@
-const Discord = require('discord.js');
- 
-const bot = new Discord.Client();
- 
-bot.on('ready', () => {
-    console.log("I am ready");
-});
- 
-bot.on('message', message => {
-    if(message.author !== bot.user) return;
- 
-    if(message.content === "hello") {
-        message.channel.sendMessage("hello");
-    }
+var Discord = require('discord.io');
+var bot = new Discord.Client({
+    autorun: true,
+    //OR
+    token: process.env.BOT_TOKEN
 });
 
-// THIS  MUST  BE  THIS  WAY
-bot.login(process.env.BOT_TOKEN);
+bot.on('ready', function() {
+    console.log(bot.username + " - (" + bot.id + ")");
+});
+
+bot.on('message', function(user, userID, channelID, message, rawEvent) {
+    if (message === "!test") { // command to trigger
+      var interval = setInterval (function (){
+        bot.sendMessage({
+          to: channelID,
+          message: "pong" // message to send
+        });
+      }, 1000); // time between each interval in milliseconds
+    }
+});
